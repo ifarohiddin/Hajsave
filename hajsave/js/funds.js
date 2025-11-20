1cb9485a15ba34e50a28d8e3475c8d601e36defa
Isomidinov Farohiddin <153911464+ifarohiddin@users.noreply.github.com>
2025-11-20 11:33:06 +0500

    added

--- hajsave/js/funds.js
+++
@@ -1,7 +0,0 @@
-/* funds.js - to'lov/ombor funksiyalari (mock) */
-export function listFunds() {
-  return [
-    { id: 1, name: 'General', balance: 1000 },
-    { id: 2, name: 'Savings', balance: 5000 },
-  ];
-}
